301d7708610257fc78712f9f89743fa265854a98
Anatoly Scherbakov <altaisoft@gmail.com>
2023-02-11 13:34:50 +0400

    Initial commit

---
+++ tests/test_jinja2.py
@@ -0,0 +1,19 @@
+import pytest
+
+from iolanta.iolanta import Iolanta
+from iolanta_jinja2.process_template import process_template
+
+
+@pytest.fixture()
+def iolanta() -> Iolanta:
+    return Iolanta().add({
+        '@id': 'john-doe',
+        'rdfs:label': 'John Doe',
+    })
+
+
+def test_simple(iolanta: Iolanta):
+    assert process_template(
+        template='Hi {{ render("john-doe") }}!',
+        iolanta=iolanta,
+    ) == 'Hi John Doe!'
